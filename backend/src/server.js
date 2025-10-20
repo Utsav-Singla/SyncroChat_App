f46e174039af5055b82e7140b46fb960dc082d83
Utsav-Singla <utsavsingla@gmail.com>
2025-10-20 17:28:05 +0530

    Some changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,8 +14,8 @@ import * as Sentry from "@sentry/node";
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(clerkMiddleware()); 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // req.auth will be available in the request object
 
 app.get("/debug-sentry", (req, res) => {
   throw new Error("My first Sentry error!");
